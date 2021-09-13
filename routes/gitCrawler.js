e9587a6a6c8568c336d0272177b701b1c7040750
SeoSiun <siun0816@hanyang.ac.kr>
2021-09-13 20:53:11 +0900

    feat: user image crawling

--- routes/gitCrawler.js
+++ routes/gitCrawler.js
@@ -4,7 +4,7 @@ var request = require('request');
 var express = require('express');
 var router = express.Router();
 
-// 잔디에서 커밋 데이터 크롤링
+// 잔디에서 데이터 크롤링
 var userName = 'gyuree-kim';
 const homeUrl = 'https://github.com/'.concat(userName);
 router.get('/', function(req, res) {
@@ -14,34 +14,44 @@ router.get('/', function(req, res) {
             console.log("error");
             res.status = 400;
         }
-        else
-        {
-            const $ = cheerio.load(html);
-            var crawledCommits = [];
-            var data;
-            var commit;
+        else{
+          const $ = cheerio.load(html);
+          var crawledCommits = [];
+          var data;
+          var commit;
+          var imageUrl;
 
-            $(".js-calendar-graph > svg > g > g > rect.ContributionCalendar-day").each(function(){
-                commit = { 'date': '', 'count': 0, 'color': 0 };
-                data = $(this);
+          // 잔디에서 커밋내역 가져오기.
+          $(".js-calendar-graph > svg > g > g > rect.ContributionCalendar-day").each(function(){
+              commit = { 'date': '', 'count': 0, 'color': 0 };
+              data = $(this);
 
-                commit['date'] = data['0']['attribs']['data-date'];
-                commit['count'] = data['0']['attribs']['data-count'];
-                commit['color'] = data['0']['attribs']['data-level'];
+              commit['date'] = data['0']['attribs']['data-date'];
+              commit['count'] = data['0']['attribs']['data-count'];
+              commit['color'] = data['0']['attribs']['data-level'];
 
-                crawledCommits.push(commit);
-            });
-            if(!crawledCommits)
-            {
-                res.status(404).json("not found");
-            }
-            else 
-            {
-                console.log(crawledCommits);
-                // res.status(200).json("crawled!");
-                // res.send(crawledCommits);
-            }
-            // console.log(data);
+              crawledCommits.push(commit);
+          });
+          if(!crawledCommits)
+          {
+              res.status(404).json("not found");
+          }
+          else 
+          {
+              console.log(crawledCommits);
+              // res.status(200).json("crawled!");
+              // res.send(crawledCommits);
+          }
+          // console.log(data);
+
+          // 유저 이미지 가져오기
+          $(".js-profile-editable-replace > div > div > a > img.avatar").each(function(){
+            data = $(this);
+
+            imageUrl = data['0']['attribs']['src'];
+
+            console.log(imageUrl);
+          });
         } 
     })
 });
