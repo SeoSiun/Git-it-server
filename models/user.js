e636802eac189970bcdac4cc349ae25c76596d40
이지원 <2019000982@hanyang.ac.kr>
2021-09-15 23:48:06 +0900

    Add api gets stats by userName

--- models/user.js
+++ models/user.js
@@ -1,4 +1,3 @@
-// import stats from "stats";
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
@@ -7,8 +6,13 @@ const user = new Schema({
   userName: {type: String, unique: true, required: true}, // user name
   school: String, // school
   friends: [{type: String, ref: this}],
-  statsId: {type: Schema.Types.ObjectId, ref: 'Stats'},
-  image: { data: Buffer, contentType: String} // profile image
+  // statsId: {type: Schema.Types.ObjectId, ref: 'Stats'},
+  image: { data: Buffer, contentType: String}, // profile image
+  tier: String, // tier by number of commits
+  totalCommits: Number, // commits for a year
+  average: Number,  // totalCommits/365
+  streak: Number, // consecutive days
+  rank: Number, // rank of shcool
 });
 
 module.exports = mongoose.model('User', user);
