424ee2e6b97068997c9d2e8ea286effbe394ca2e
SeoSiun <siun0816@hanyang.ac.kr>
2021-09-30 00:25:16 +0900

    test

--- routes/user.js
+++ routes/user.js
@@ -67,15 +67,15 @@ router.get('/:userName/stats', (req, res) => {
 router.post('/', (req, res) => {
   isUserInGithub(req.body.userName, function(isExist){
     if(isExist){
-      var user = new User();
       User.insertMany({userName: req.body.userName}, (err, user) => {
         if(err) res.status(500).json({error: `db failure`});
         else {
-          getCommitByCrawling(req.body.userName, function(result){
-            if(result === null) return res.status(404).json({msg: `user not found in gitHub`});
-            console.log('add user 성공');
-            return res.status(200).json({result: 1});
-          });
+          // getCommitByCrawling(req.body.userName, function(result){
+          //   // if(result === null) return res.status(404).json({msg: `user not found in gitHub!!!`});
+          //   console.log('add user 성공');
+          //   return res.status(200).json({result: 1});
+          // });
+          return res.status(200).json({result: 1});
         }
       })
     }
