1ad6b5bdb478868e84a759020234bb2385ef0033
SeoSiun <siun0816@hanyang.ac.kr>
2021-10-02 00:32:50 +0900

    .

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,12 @@
 const express = require('express');
-
 const router = express.Router();
-
 const User = require('./user');
-
 const {getCommitByCrawling} = require('./gitCrawler.js');
 
 
-
-// router.use('/git-crawler', GitCrawler); //router.use({api url}, {var name})
-
 router.use('/user', User);
 
-// get a friendsInfo by friendName
+// get a commitInfo by name
 router.get('/commit/:userName', (req, res) => {
   getCommitByCrawling(req.params.userName, function(result){
     return res.status(200).json(result);
