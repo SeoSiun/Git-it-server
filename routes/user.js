f283ca6e590558605d3c176aa5b5faa2ba5e23dd
SeoSiun <siun0816@hanyang.ac.kr>
2021-10-02 00:15:23 +0900

    순서 바꾸기

--- routes/user.js
+++ routes/user.js
@@ -5,32 +5,6 @@ var router = express.Router();
 
 const User = require('../models/user.js');
 
-// get a user by userName
-router.get('/:userName', (req, res) => {
-  const filter = {userName: req.params.userName};
-  getCommitByCrawling(req.params.userName, function(result){
-    if(result===null) res.status(200).json({validation: 3});
-    else{
-      User.findOne(filter, (err, user) => {
-        if(err) res.status(200).json({validation: 0});
-        else if(!user) res.status(200).json({validation: 2});
-        else {
-          console.log("get User by userName 성공");
-
-          return res.status(200).json({
-            validation: 1,
-            userName: req.params.userName,
-            commitsRecord: result["crawledCommits"],
-            profileImageUrl: user["imageUrl"],
-            streak: user["streak"],
-            friendList: user["friends"]
-          });
-        }
-      });
-    }
-  })
-})
-
 // get a friendsInfo by friendName
 router.get('/:friendName/friend', (req, res) => {
   getCommitByCrawling(req.params.friendName, function(result){
@@ -81,6 +55,32 @@ router.get('/userName/:userName/friends', (req, res) => {
     else {
       console.log('getFreinds by userName 성공');
       return res.status(200).json({friendList});
+    }
+  })
+})
+
+// get a user by userName
+router.get('/:userName', (req, res) => {
+  const filter = {userName: req.params.userName};
+  getCommitByCrawling(req.params.userName, function(result){
+    if(result===null) res.status(200).json({validation: 3});
+    else{
+      User.findOne(filter, (err, user) => {
+        if(err) res.status(200).json({validation: 0});
+        else if(!user) res.status(200).json({validation: 2});
+        else {
+          console.log("get User by userName 성공");
+
+          return res.status(200).json({
+            validation: 1,
+            userName: req.params.userName,
+            commitsRecord: result["crawledCommits"],
+            profileImageUrl: user["imageUrl"],
+            streak: user["streak"],
+            friendList: user["friends"]
+          });
+        }
+      });
     }
   })
 })
