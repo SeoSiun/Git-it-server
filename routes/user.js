bea54ddec7ab3402525d5a8b155aed7ba706a0b7
Rudolf0328 <54528011+Rudolf0328@users.noreply.github.com>
2021-10-02 16:20:32 +0900

    Api friends (#13)
    update friend api, delete friend api complete

--- routes/user.js
+++ routes/user.js
@@ -66,13 +66,31 @@ router.post('/', (req, res) => {
 
 /* -------------------- Update Api -------------------- */
 
-// update friends by userName
+// update friend by json
 router.put('/friend/add', (req, res) => {
   const name = req.body.userName;            // body로 받은 userName을 저장
   const fName = req.body.friendName;    // body로 받은 friendName을 저장
   User.updateOne(
     {userName: name},                         // 이름이 name인 사람을 찾아서햐
     {$push: {friends: fName}},          // friendName을 friends 배열에 추가해라
+    function (error, success) {
+      if(error) {
+        console.log(error);
+        return res.status(400).json("0");
+      } else {
+        console.log(success);
+        return res.status(200).json("1");
+      }
+    }).exec()
+})
+
+// delete friend by json
+router.put('/friend/delete', (req, res) => {
+  const name = req.body.userName;            // body로 받은 userName을 저장
+  const fName = req.body.friendName;    // body로 받은 friendName을 저장
+  User.updateOne(
+    {userName: name},                         // 이름이 name인 사람을 찾아서햐
+    {$pull: {friends: fName}},          // friendName을 friends 배열에서 삭제하라
     function (error, success) {
       if(error) {
         console.log(error);
