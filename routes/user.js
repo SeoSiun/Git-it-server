88eaa55f1a4b97df3370be71dedb4772251a2c48
Rudolf0328 <54528011+Rudolf0328@users.noreply.github.com>
2021-10-02 16:38:39 +0900

    Api stats (#14)
    get stats api update

--- routes/user.js
+++ routes/user.js
@@ -38,15 +38,22 @@ router.get('/github/:userName', (req, res) => {
   })
 })
 
-// get totalCommits by userName
+// get stats by userName
 router.get('/stats/:userName', (req, res) => {
   const filter = {userName: req.params.userName};
-  User.findOne(filter).select('tier totalCommits average streak tier rank').exec((err, stats) => {
-    if(err) res.status(500).json({error: `db failure`});
-    if(!stats) res.status(404).json({msg: `stats not found`});
+  User.findOne(filter).exec((err, stats) => {
+    if(err) res.status(500).json({"validation": 0});      // db failed -> db가 아예 터지거나 쿼리가 잘못되었을 때
+    if(!stats) res.status(404).json({"validation": 2});   // no user  -> db에 저장된 유저정보가 없는 이름일 때
     else {
       console.log('get stats by user name 성공');
-      return res.status(200).json(stats);
+      return res.status(200).json({
+        validation: 1,
+        average: stats["average"],
+        streak: stats["streak"],
+        tier: stats["tier"],
+        totalCommits: stats["totalCommits"]
+        // rank: stats["rank"]          학교 랭크인데 api list에 없음 한다는겨 만다는겨
+      });
     }
   })
 })
@@ -76,10 +83,28 @@ router.put('/friend/add', (req, res) => {
     function (error, success) {
       if(error) {
         console.log(error);
-        return res.status(400).json("0");
+        return res.status(400).json({"result": 0});
       } else {
         console.log(success);
-        return res.status(200).json("1");
+        return res.status(200).json({"result": 1});
+      }
+    }).exec()
+})
+
+// delete friend by json
+router.put('/friend/delete', (req, res) => {
+  const name = req.body.userName;            // body로 받은 userName을 저장
+  const fName = req.body.friendName;    // body로 받은 friendName을 저장
+  User.updateOne(
+    {userName: name},                         // 이름이 name인 사람을 찾아서햐
+    {$pull: {friends: fName}},          // friendName을 friends 배열에서 삭제하라
+    function (error, success) {
+      if(error) {
+        console.log(error);
+        return res.status(400).json({"result": 0});
+      } else {
+        console.log(success);
+        return res.status(200).json({"result": 1});
       }
     }).exec()
 })
