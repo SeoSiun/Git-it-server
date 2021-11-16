dc4b65515b48be173d89e1a676ba2b654f17fa64
SeoSiun <siun0816@hanyang.ac.kr>
2021-11-17 01:03:30 +0900

    1분마다 전체 유저의 totalCommit 업데이트

--- app.js
+++ app.js
@@ -12,14 +12,12 @@ const User = require('./models/user.js');
 function updateUsers(){
   console.log("call updateUsers()");
   User.find({},(err, users) =>{
-    console.log("??");
     if(err){
       console.log("err")
     }
     else{
       console.log("user 목록 가져오기 성공!")
       users.forEach(function(user, index, _users){
-        console.log(user["userName"]);
         getCommitByCrawling(user["userName"],function(result){
           // user["school"]에 따라 해당하는 school에 result["totalommit"]을 더해주기.
         })
